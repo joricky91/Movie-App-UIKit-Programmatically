1f2d0d5d5d122cc4b1835f92f3e310315a69e978
joricky91 <jonathanricky91@gmail.com>
2023-01-31 10:35:38 +0700

    feat: creating 3 collection view embedded inside a scrollView

---
+++ Movie App UIKit Programmatically/CustomCell.swift
@@ -0,0 +1,55 @@
+//
+//  CustomCell.swift
+//  Movie App UIKit Programmatically
+//
+//  Created by Jonathan Ricky Sandjaja on 31/01/23.
+//
+
+import UIKit
+
+class CustomCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupViews()
+        setupConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    lazy var moviePoster: UIImageView = {
+        let image = UIImageView()
+        image.image = UIImage(named: "your_name")
+        image.contentMode = .scaleToFill
+        image.clipsToBounds = true
+        image.translatesAutoresizingMaskIntoConstraints = false
+        return image
+    }()
+    
+    lazy var movieTitle: UILabel = {
+        let label = UILabel()
+        label.text = "Kimi no na wa"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    func setupViews() {
+        contentView.addSubview(moviePoster)
+        contentView.addSubview(movieTitle)
+    }
+    
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            moviePoster.topAnchor.constraint(equalTo: contentView.topAnchor),
+            moviePoster.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            moviePoster.widthAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 1),
+            moviePoster.heightAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 1.3),
+            
+            movieTitle.topAnchor.constraint(equalTo: moviePoster.bottomAnchor, constant: 3),
+            movieTitle.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            movieTitle.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+        ])
+    }
+}
--- Movie App UIKit Programmatically/ViewController.swift
+++ Movie App UIKit Programmatically/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +18,20 @@ class ViewController: UIViewController, UICollectionViewDelegateFlowLayout, UICo
         title = "Movie"
         navigationController?.navigationBar.prefersLargeTitles = true
     }
+    
+    lazy var scrollView: UIScrollView = {
+        let scroll = UIScrollView()
+        scroll.isScrollEnabled = true
+        scroll.translatesAutoresizingMaskIntoConstraints = false
+        return scroll
+    }()
+    
+    lazy var contentView: UIStackView = {
+        let view = UIStackView()
+        view.axis = .vertical
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
     
     lazy var titleLabel: UILabel = {
         let label = UILabel()
@@ -34,14 +48,23 @@ class ViewController: UIViewController, UICollectionViewDelegateFlowLayout, UICo
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
+    
+    lazy var titleLabel3: UILabel = {
+        let label = UILabel()
+        label.text = "Top Rated"
+        label.font = UIFont.systemFont(ofSize: 22, weight: .bold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
 
     lazy var collectionView1: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
-        layout.itemSize = CGSize(width: 120, height: 170)
+        layout.itemSize = CGSize(width: 150, height: 220)
         layout.scrollDirection = .horizontal
         let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.showsHorizontalScrollIndicator = false
         cv.translatesAutoresizingMaskIntoConstraints = false
-        cv.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        cv.register(CustomCell.self, forCellWithReuseIdentifier: "cell")
         cv.dataSource = self
         cv.delegate = self
         return cv
@@ -49,64 +72,94 @@ class ViewController: UIViewController, UICollectionViewDelegateFlowLayout, UICo
     
     lazy var collectionView2: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
-        layout.itemSize = CGSize(width: 120, height: 170)
+        layout.itemSize = CGSize(width: 150, height: 220)
         layout.scrollDirection = .horizontal
         let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.showsHorizontalScrollIndicator = false
         cv.translatesAutoresizingMaskIntoConstraints = false
-        cv.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell2")
+        cv.register(CustomCell.self, forCellWithReuseIdentifier: "cell2")
         cv.dataSource = self
         cv.delegate = self
         return cv
     }()
     
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        if collectionView == collectionView1 {
-            return 10
-        }
-        
-        return 10
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        if collectionView == collectionView1 {
-            let cell1 = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
-            cell1.backgroundColor = .red
-            return cell1
-        } else {
-            let cell2 = collectionView.dequeueReusableCell(withReuseIdentifier: "cell2", for: indexPath)
-            cell2.backgroundColor = .blue
-            return cell2
-        }
-    }
+    lazy var collectionView3: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: 150, height: 220)
+        layout.scrollDirection = .horizontal
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.showsHorizontalScrollIndicator = false
+        cv.translatesAutoresizingMaskIntoConstraints = false
+        cv.register(CustomCell.self, forCellWithReuseIdentifier: "cell3")
+        cv.dataSource = self
+        cv.delegate = self
+        return cv
+    }()
     
     func setupViews() {
-        view.addSubview(titleLabel)
-        view.addSubview(collectionView1)
-        view.addSubview(titleLabel2)
-        view.addSubview(collectionView2)
+        view.addSubview(scrollView)
+        scrollView.addSubview(contentView)
+        contentView.addArrangedSubview(titleLabel)
+        contentView.addArrangedSubview(collectionView1)
+        contentView.addArrangedSubview(titleLabel2)
+        contentView.addArrangedSubview(collectionView2)
+        contentView.addArrangedSubview(titleLabel3)
+        contentView.addArrangedSubview(collectionView3)
     }
     
     func setupConstraints() {
         let safeArea = view.safeAreaLayoutGuide
         
         NSLayoutConstraint.activate([
-            titleLabel.topAnchor.constraint(equalTo: safeArea.topAnchor),
-            titleLabel.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 16),
+            scrollView.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor),
+            
+            contentView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            contentView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
+            
+            titleLabel.topAnchor.constraint(equalTo: contentView.topAnchor),
             
             collectionView1.topAnchor.constraint(equalTo: titleLabel.bottomAnchor),
-            collectionView1.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 16),
-            collectionView1.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -16),
-            collectionView1.heightAnchor.constraint(equalTo: collectionView1.widthAnchor, multiplier: 0.5),
-            
-            titleLabel2.topAnchor.constraint(equalTo: collectionView1.bottomAnchor, constant: 16),
-            titleLabel2.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor),
+            collectionView1.heightAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 0.65),
             
             collectionView2.topAnchor.constraint(equalTo: titleLabel2.bottomAnchor),
-            collectionView2.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 16),
-            collectionView2.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -16),
-            collectionView2.heightAnchor.constraint(equalTo: collectionView2.widthAnchor, multiplier: 0.5),
+            collectionView2.heightAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 0.65),
+            
+            collectionView3.topAnchor.constraint(equalTo: titleLabel3.bottomAnchor),
+            collectionView3.heightAnchor.constraint(equalTo: contentView.widthAnchor, multiplier: 0.65),
         ])
     }
 
+}
+
+
+extension ViewController: UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        if collectionView == collectionView1 {
+            return 10
+        } else if collectionView == collectionView2 {
+            return 10
+        } else {
+            return 10
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if collectionView == collectionView1 {
+            let cell1 = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CustomCell
+            return cell1
+        } else if collectionView == collectionView2 {
+            let cell2 = collectionView.dequeueReusableCell(withReuseIdentifier: "cell2", for: indexPath) as! CustomCell
+            return cell2
+        } else {
+            let cell3 = collectionView.dequeueReusableCell(withReuseIdentifier: "cell3", for: indexPath) as! CustomCell
+            return cell3
+        }
+    }
 }
 
